5768cc04d525e5b97728540d9a6b77e2bac8ffa4
KreitinnSoftware <pablopro5051@gmail.com>
2024-03-02 17:01:00 -0300

    Add some flags to more packages

--- packages/03-xorgproto/build.sh
+++ packages/03-xorgproto/build.sh
@@ -1,1 +1,2 @@
 SRC_URL=https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2023.2.tar.xz
+CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE"
--- packages/04-xtrans/build.sh
+++ packages/04-xtrans/build.sh
@@ -1,1 +1,2 @@
 SRC_URL=https://xorg.freedesktop.org/releases/individual/lib/xtrans-1.5.0.tar.xz
+CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE"
