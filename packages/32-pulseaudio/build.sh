dd4737496fb7b2e950783b70a03d02aa39a936d9
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2024-03-04 16:42:36 -0300

    Disable glib for 'pulseaudio'

--- packages/32-pulseaudio/build.sh
+++ packages/32-pulseaudio/build.sh
@@ -1,5 +1,5 @@
 SRC_URL=https://github.com/pulseaudio/pulseaudio/archive/refs/tags/v17.0.zip
-MESON_ARGS="--cross-file=../../../packages/32-pulseaudio/meson-cross-file-aarch64 -Dbashcompletiondir=$PREFIX/share/bash-completion -Dalsa=disabled -Dx11=disabled -Dgtk=disabled -Dopenssl=disabled -Dgsettings=disabled -Ddoxygen=false -Ddatabase=simple -Dtests=false"
+MESON_ARGS="--cross-file=../../../packages/32-pulseaudio/meson-cross-file-aarch64 -Dglib=disabled -Dbashcompletiondir=$PREFIX/share/bash-completion -Dalsa=disabled -Dx11=disabled -Dgtk=disabled -Dopenssl=disabled -Dgsettings=disabled -Ddoxygen=false -Ddatabase=simple -Dtests=false"
 USE_NDK_VERSION=25b
 RUN_POST_APPLY_PATCH="cp include/libintl.h $PREFIX/include"
 CFLAGS="-I$PREFIX/include"
