d79dbbd683965a7e82be0af100d758c44a03e900
KreitinnSoftware <pablopro5051@gmail.com>
2024-03-07 09:34:44 -0300

    Disable zstd for 'mesa' and set NDK version to 26b

--- packages/36-mesa/build.sh
+++ packages/36-mesa/build.sh
@@ -1,3 +1,3 @@
 SRC_URL=https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.tar.gz
-MESON_ARGS="--cross-file=../../../meson-cross-file-aarch64 -Dgallium-drivers=swrast,virgl -Dvulkan-drivers= -Dglx=dri -Ddri3=enabled -Dplatforms=x11 -Dcpp_rtti=false -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=enabled -Dshared-llvm=disabled"
-USE_NDK_VERSION=25b
+MESON_ARGS="--cross-file=../../../meson-cross-file-aarch64 -Dgallium-drivers=swrast,virgl -Dvulkan-drivers= -Dglx=dri -Ddri3=enabled -Dplatforms=x11 -Dcpp_rtti=false -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=enabled -Dshared-llvm=disabled -Dzstd=disabled"
+USE_NDK_VERSION=26b
