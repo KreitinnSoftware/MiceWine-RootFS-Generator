908f872ffdeda10c467917fc2e402be6189463ae
KreitinnSoftware <pablopro5051@gmail.com>
2024-05-09 22:26:34 -0300

    Use Specific Commit Until Update Patch for 'mesa'

--- packages/59-mesa/build.sh
+++ packages/59-mesa/build.sh
@@ -1,4 +1,5 @@
-SRC_URL=https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.tar.gz
+GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
+GIT_COMMIT=5eb0dec525a2e368d7b2a4cb79264926b51d8f88
 MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCHITECTURE -Dgallium-drivers=swrast,zink,virgl -Dvulkan-drivers=freedreno,virtio -Dfreedreno-kmds=kgsl,msm -Dglvnd=true -Dglx=dri -Ddri3=enabled -Dplatforms=x11 -Dcpp_rtti=false -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=enabled -Dshared-llvm=disabled -Dzstd=enabled"
 USE_NDK_VERSION=26b
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
