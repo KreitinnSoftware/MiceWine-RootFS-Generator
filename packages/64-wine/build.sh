795cae8f4a45df0d28b1e4f0812df3081900f496
KreitinnSoftware <pablopro5051@gmail.com>
2024-08-17 14:40:55 -0300

    Add OVERRIDE_PREFIX for 'wine' for place all wine libraries on separate folder

--- packages/64-wine/build.sh
+++ packages/64-wine/build.sh
@@ -4,11 +4,12 @@ BLACKLIST_ARCHITECTURE=aarch64
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
+OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
 CONFIGURE_ARGS="enable_wineandroid_drv=no \
 				--enable-archs=i386,x86_64 \
 				--host=$TOOLCHAIN_TRIPLE \
 				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
-				--prefix=$PREFIX/wine \
+				--prefix=$OVERRIDE_PREFIX \
 				--without-oss \
 				--disable-winemenubuilder \
 				--disable-win16 \
