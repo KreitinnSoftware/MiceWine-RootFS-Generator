34f737e1da9c0ffacde0851f51915eda57527c44
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2024-08-19 01:44:39 -0300

    Update build.sh

--- packages/57-mesa-virgl/build.sh
+++ packages/57-mesa-virgl/build.sh
@@ -2,4 +2,4 @@ GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
 GIT_COMMIT=0a3784ae33bbdcd50b2a6d8de5d52779276bb36c
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 OVERRIDE_PREFIX=$PREFIX/mesa-virgl
-MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCHITECTURE -Dgallium-drivers=virgl -Dvulkan-drivers= -Dglvnd=enabled -Dglx=dri -Ddri3=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=enabled -Dzstd=enabled"
+MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCHITECTURE -Dgallium-drivers=virgl -Dvulkan-drivers= -Dglvnd=true -Dglx=dri -Ddri3=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=enabled -Dzstd=enabled"
