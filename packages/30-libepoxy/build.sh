248d24d61824c858933f43abfe82b1feee7fe7cd
KreitinnSoftware <pablopro5051@gmail.com>
2024-08-25 23:08:03 -0300

    Remove VirGL Packages

--- packages/30-libepoxy/build.sh
+++
@@ -1,2 +0,0 @@
-SRC_URL=https://github.com/anholt/libepoxy/archive/refs/tags/1.5.10.tar.gz
-MESON_ARGS="--cross-file=../../../meson-cross-file-aarch64 -Degl=yes -Dglx=no -Dx11=false --libdir lib -Ddefault_library=static"
--- packages/31-virglrenderer/build.sh
+++
@@ -1,3 +0,0 @@
-SRC_URL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-0.10.4/virglrenderer-virglrenderer-0.10.4.tar.gz
-MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCHITECTURE --libdir lib -Degl_without_gbm=true -Dplatforms=egl"
-CFLAGS="-I$PREFIX/include"
