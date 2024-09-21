d656710db8469ab7e6d42d769a367fa2446af1e5
Pablo Labs <pablolabs36@gmail.com>
2024-09-20 21:13:01 -0300

    Update Mesa Patches, Update Vulkan Spec to 1.3.295

--- packages/31-Vulkan-Headers/build.sh
+++ packages/31-Vulkan-Headers/build.sh
@@ -1,2 +1,2 @@
-SRC_URL=https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.288.tar.gz
+SRC_URL=https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.295.tar.gz
 CMAKE_ARGS=" "
--- packages/33-Vulkan-Volk/build.sh
+++ packages/33-Vulkan-Volk/build.sh
@@ -1,2 +1,2 @@
-SRC_URL=https://github.com/zeux/volk/archive/refs/tags/1.3.270.tar.gz
+SRC_URL=https://github.com/zeux/volk/archive/refs/tags/1.3.295.tar.gz
 CMAKE_ARGS="-DVOLK_INSTALL=ON"
