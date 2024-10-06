4b1c372666e19ad19b36e5924817203ec3830ba7
KreitinnSoftware <pablolabs36@gmail.com>
2024-10-06 16:58:06 -0300

    Update '30-libdrm' to version 2.4.123, Enable AMD DRM

--- packages/30-libdrm/build.sh
+++ packages/30-libdrm/build.sh
@@ -1,2 +1,2 @@
-SRC_URL=https://dri.freedesktop.org/libdrm/libdrm-2.4.120.tar.xz
-MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCHITECTURE -Dfreedreno=enabled -Dfreedreno-kgsl=true -Dintel=disabled -Dradeon=disabled -Damdgpu=disabled -Dnouveau=disabled -Dvmwgfx=disabled -Dtests=false"
+SRC_URL=https://dri.freedesktop.org/libdrm/libdrm-2.4.123.tar.xz
+MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCHITECTURE -Dfreedreno=enabled -Dfreedreno-kgsl=true -Dintel=disabled -Dradeon=enabled -Damdgpu=enabled -Dnouveau=disabled -Dvmwgfx=disabled -Dtests=false"
