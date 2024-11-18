9073e7a58a028ba87eaa60ede4fac4b2cc08b16f
KreitinnSoftware <pablopro36@gmail.com>
2024-11-18 16:09:33 -0300

    Fix Flags for mesa-panfrost

--- packages/69-mesa-panfrost/build.sh
+++ packages/69-mesa-panfrost/build.sh
@@ -1,4 +1,4 @@
 PKG_VER=[gss]
 GIT_URL=https://github.com/joaobatista070/mesa-Panfork-android
 LDFLAGS="-L$PREFIX/lib -landroid-shmem -lxcb-xfixes"
-MESON_ARGS="-Dgallium-drivers=panfrost -Dvulkan-drivers= -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled"
+MESON_ARGS="-Dgallium-drivers=panfrost -Dvulkan-drivers= -Dglvnd=true -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled"
