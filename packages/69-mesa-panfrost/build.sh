96eae02988f09ea953acfa4e364f3a3cad4961fa
KreitinnSoftware <pablopro36@gmail.com>
2024-11-18 17:24:31 -0300

    Try (again) to fix mesa-panfrost

--- packages/69-mesa-panfrost/build.sh
+++ packages/69-mesa-panfrost/build.sh
@@ -1,4 +1,4 @@
 PKG_VER=[gss]
 GIT_URL=https://github.com/joaobatista070/mesa-Panfork-android
-LDFLAGS="-L$PREFIX/lib -landroid-shmem -lxcb-xfixes"
+LDFLAGS="-L$PREFIX/lib -landroid-shmem -lxcb-xfixes -Wl,--undefined-version"
 MESON_ARGS="-Dgallium-drivers=panfrost,swrast -Dvulkan-drivers= -Dglvnd=true -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled"
