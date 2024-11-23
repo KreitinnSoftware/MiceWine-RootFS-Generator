653ee47aa270e40499af695f805cf468d70d641b
PabloLabs36 <pablolabs36@gmail.com>
2024-11-23 13:48:56 -0300

    Just add a blank space for rebuilding 'mesa-zink' (Symlinks Broken)

--- packages/58-mesa-zink/build.sh
+++ packages/58-mesa-zink/build.sh
@@ -5,4 +5,4 @@ PKG_PRETTY_NAME="Mesa Zink Driver"
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
 GIT_COMMIT=ed64ecc
 LDFLAGS="-L$PREFIX/lib -landroid-shmem -lxcb-xfixes"
-MESON_ARGS="-Dgallium-drivers=zink -Dvulkan-drivers= -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled"
+MESON_ARGS="-Dgallium-drivers=zink -Dvulkan-drivers= -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled" 
