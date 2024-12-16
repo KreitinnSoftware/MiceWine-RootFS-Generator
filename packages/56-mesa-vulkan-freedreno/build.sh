63bfb46af788e9cc61aa70f6a4b61c9d307b5f7a
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2024-12-16 16:13:04 -0300

    Update Turnip

--- packages/56-mesa-vulkan-freedreno/build.sh
+++ packages/56-mesa-vulkan-freedreno/build.sh
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_freedreno.so"
 BLACKLIST_ARCHITECTURE=x86_64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=8a453669e2bd44651cacdeac1c372d6e80cab0a7
+GIT_COMMIT=4d04396531a25986b444d064434d6a9272c70d8b
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
