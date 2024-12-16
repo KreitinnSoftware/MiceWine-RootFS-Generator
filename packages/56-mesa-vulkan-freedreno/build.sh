3ed825845c168375c82dd33ce714137f4d5d70c3
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2024-12-16 20:41:33 -0300

    Enable msm for turnip

--- packages/56-mesa-vulkan-freedreno/build.sh
+++ packages/56-mesa-vulkan-freedreno/build.sh
@@ -9,4 +9,4 @@ GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
 GIT_COMMIT=4d04396531a25986b444d064434d6a9272c70d8b
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
-MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
+MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
