0655ee3f01d3292c015a1e2d74d441eeb6a8d666
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-01-01 22:56:22 -0300

    Revert Wine to 9.0 (Just for testing, I will delete this later)

--- packages/67-wine/build.sh
+++ packages/67-wine/build.sh
@@ -1,11 +1,11 @@
-PKG_VER="10.0-rc3-esync"
+PKG_VER="9.0-esync"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=2ca04fd1e5566b063dc7c89bf3731e478595093c
+GIT_COMMIT=1c3bdc015b17ed4b5f4e4ef954ebede30f0fce18
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
