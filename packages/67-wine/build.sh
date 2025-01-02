764101d7231637f8d109d91ed824a118b6432c66
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-01-02 12:06:14 -0300

    I just want to compile a wine for a friend run on termux I will delete this later

--- packages/67-wine/build.sh
+++ packages/67-wine/build.sh
@@ -5,7 +5,7 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=2ca04fd1e5566b063dc7c89bf3731e478595093c
+GIT_COMMIT=87878c9bf9d577f5a81369bdaaa0f7056339b5fc
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
