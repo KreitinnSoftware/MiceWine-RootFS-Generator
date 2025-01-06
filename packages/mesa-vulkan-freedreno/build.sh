1155738e92903060bcdc6d4e9cbb38225a45dcfb
KreitinnSoftware <pablolabs36@gmail.com>
2025-01-06 19:14:27 -0300

    Update 'mesa-vulkan-freedreno' and 'mesa-vulkan-radeon'

--- packages/mesa-vulkan-freedreno/build.sh
+++ packages/mesa-vulkan-freedreno/build.sh
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_freedreno.so"
 BLACKLIST_ARCHITECTURE=x86_64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=4d04396531a25986b444d064434d6a9272c70d8b
+GIT_COMMIT=61593e9c549e6f497fd7344aa353663e10354058
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
--- packages/mesa-vulkan-radeon/build.sh
+++ packages/mesa-vulkan-radeon/build.sh
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_radeon.so"
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=8a453669e2bd44651cacdeac1c372d6e80cab0a7
+GIT_COMMIT=61593e9c549e6f497fd7344aa353663e10354058
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=amd -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
