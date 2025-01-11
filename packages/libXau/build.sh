0d6b08c1420309c64d6add8fdfa8a8c21da7fe10
KreitinnSoftware <pablolabs36@gmail.com>
2025-01-10 22:32:23 -0300

    Typo on libXau

--- packages/libXau/build.sh
+++ packages/libXau/build.sh
@@ -1,3 +1,3 @@
 PKG_VER=1.0.12
-SRC_URL=https://xorg.freedesktop.org/releases/individual/lib/libXau-$PKG_VER.tar.xz
+SRC_URL=https://xorg.freedesktop.org/releases/individual/lib/libXau-$PKG_VER.tar.gz
 CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE --disable-static"
