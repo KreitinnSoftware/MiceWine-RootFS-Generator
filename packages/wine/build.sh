13d3ce5cbc059f523b08680fcf21b5ff12fadf24
KreitinnSoftware <pablolabs36@gmail.com>
2025-01-21 20:02:45 -0300

    Update Wine for 10.0

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,11 +1,11 @@
-PKG_VER="10.0-rc3-esync"
+PKG_VER="10.0-esync"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=2ca04fd1e5566b063dc7c89bf3731e478595093c
+GIT_COMMIT=f055b615d87c987bdb156fd6d29b4eff00cd8d85
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
