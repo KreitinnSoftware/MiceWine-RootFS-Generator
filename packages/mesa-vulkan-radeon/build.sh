8c199f3d76ab8be9494b9401b42232f9e3841484
KreitinnSoftware <pablolabs36@gmail.com>
2025-01-22 09:37:10 -0300

    Update Mesa

--- packages/mesa-vulkan-radeon/build.sh
+++ packages/mesa-vulkan-radeon/build.sh
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_radeon.so"
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=61593e9c549e6f497fd7344aa353663e10354058
+GIT_COMMIT=365cd04375baeb8c607c1a49e85e9a364e193a08
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=amd -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
