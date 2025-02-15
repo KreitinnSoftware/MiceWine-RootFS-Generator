0fa26951f88f06c675c388baa537269c1bd0d613
KreitinnSoftware <pablolabs36@gmail.com>
2025-02-15 15:41:21 -0300

    Add AAudio-Sink on 'pulseaudio', and let app handle with 'default.pa'

--- packages/pulseaudio/post-install.sh
+++ packages/pulseaudio/post-install.sh
@@ -2,8 +2,3 @@ cd $DESTDIR/$PREFIX/lib
 for lib in pulseaudio/{,modules/}lib*.so*; do
 	ln -v -s -f "$lib" "$(basename "$lib")"
 done
-
-# Pulseaudio fails to start when it cannot detect any sound hardware
-# so disable hardware detection.
-sed -i $DESTDIR/$PREFIX/etc/pulse/default.pa -e '/^load-module module-detect$/s/^/#/'
-echo "load-module module-sles-sink" >> $DESTDIR/$PREFIX/etc/pulse/default.pa
