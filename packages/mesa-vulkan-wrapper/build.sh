efbbe670dbe4c0af2f5cb2f6d201d50ec0d03b49
KreitinnSoftware <pablolabs36@gmail.com>
2025-02-16 12:35:26 -0300

    Update 'mesa-vulkan-wrapper' and add a patch for fixing memory errors on 32bits games and always enable bc

--- packages/mesa-vulkan-wrapper/build.sh
+++ packages/mesa-vulkan-wrapper/build.sh
@@ -4,6 +4,6 @@ PKG_PRETTY_NAME="Mesa Android Wrapper"
 VK_DRIVER_LIB="libvulkan_wrapper.so"
 
 GIT_URL=https://github.com/xMeM/mesa
-GIT_COMMIT=d7a274ab77ecadb4b06175c38b726d24a6cb9210
+GIT_COMMIT=e65c7eb6ee2f9903c3256f2677beb1d98464103f
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
