64543faf8e2e85554c6093b18a41678ae54fb2f7
KreitinnSoftware <pablolabs36@gmail.com>
2025-02-19 22:53:27 -0300

    Update Wine, Try to enable xshm

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,15 +1,17 @@
-PKG_VER="10.0-esync"
+PKG_VER="10.1-esync"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=f055b615d87c987bdb156fd6d29b4eff00cd8d85
+GIT_COMMIT=21db52b0934844bec8b2f6660a393445fb240f18
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
 OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
+LDFLAGS="-L$PREFIX/lib -landroid-shmem"
+CFLAGS="-I$PREFIX/include"
 CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
 				--host=$TOOLCHAIN_TRIPLE \
 				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
@@ -21,11 +23,12 @@ CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
 				--with-x \
 				--x-libraries=$PREFIX/lib \
 				--x-includes=$PREFIX/include \
+				--with-mingw=gcc \
 				--with-pulse \
 				--with-gstreamer \
 				--with-opengl \
 				--with-gnutls \
-				--without-xshm \
+				--with-xshm \
 				--without-xxf86vm \
 				--without-osmesa \
 				--without-usb \
