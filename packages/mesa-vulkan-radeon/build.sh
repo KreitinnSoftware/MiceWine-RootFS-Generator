acf7f30016c1c82baf80bee49ff10d357a6ea1a7
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-03-04 09:46:05 -0300

    Update Mesa Radeon for 25.0.0 Stable

--- packages/mesa-vulkan-radeon/build.sh
+++ packages/mesa-vulkan-radeon/build.sh
@@ -1,12 +1,11 @@
-PKG_VER=25.0.0-devel-[gss]
+PKG_VER=25.0.0
 PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Radeon Driver"
 VK_DRIVER_LIB="libvulkan_radeon.so"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
-GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=365cd04375baeb8c607c1a49e85e9a364e193a08
+SRC_URL=https://archive.mesa3d.org/mesa-$PKG_VER.tar.xz
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=amd -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
