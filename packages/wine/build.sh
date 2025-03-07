93397ee221887c1c39385271a30d8d7d4c94267a
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-03-07 18:27:10 -0300

    Update Wine from My Repo

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,11 +1,11 @@
-PKG_VER="10.1-esync-xinput"
+PKG_VER="10.1-1-esync-xinput"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=3013a4648fa75609ea6977df09f43eaa5592babd
+GIT_COMMIT=44356d79e788068e8200c9a582a812fabe76865c
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
