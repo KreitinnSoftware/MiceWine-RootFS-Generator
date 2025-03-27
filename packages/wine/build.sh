5b78b38d9eaef03b70e3ea5b563de00baec61d9f
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-03-26 23:15:37 -0300

    I have forgot most important part the commit

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -5,7 +5,7 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 BLACKLIST_ARCHITECTURE=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=68daea43962992b6f09f4771dd815f9974505923
+GIT_COMMIT=6ec34b454913b18a45eb75030f53a81e3d6a474f
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
