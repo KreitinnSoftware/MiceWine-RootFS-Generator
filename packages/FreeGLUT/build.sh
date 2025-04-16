c05eb07984d38274f433db707c09713094d4a0fc
KreitinnSoftware <pablolabs36@gmail.com>
2025-04-16 20:20:18 -0300

    Fix 'FreeGLUT' build

--- packages/FreeGLUT/build.sh
+++ packages/FreeGLUT/build.sh
@@ -2,3 +2,4 @@ PKG_VER=3.4.0
 SRC_URL=https://downloads.sourceforge.net/freeglut/freeglut-$PKG_VER.tar.gz
 CMAKE_ARGS="-DANDROID=OFF -DCMAKE_LIBRARY_PATH=/data/data/com.micewine.emu/files/usr/lib"
 CFLAGS="-I$PREFIX_DIR/include"
+LDFLAGS="-L$PREFIX_DIR/lib -lGL"
