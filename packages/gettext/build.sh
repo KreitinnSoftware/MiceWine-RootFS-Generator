1c676a895aa4394aab28ef1882d8c58a67fcf1cc
KreitinnSoftware <pablolabs36@gmail.com>
2025-04-16 12:13:18 -0300

    Add 'gettext' and remove 'fakeintl'

---
+++ packages/gettext/build.sh
@@ -0,0 +1,3 @@
+PKG_VER=0.24
+SRC_URL=https://mirrors.kernel.org/gnu/gettext/gettext-$PKG_VER.tar.xz
+CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE ac_cv_have_decl_posix_spawn=no ac_cv_header_spawn_h=no gl_cv_func_working_error=yes gl_cv_terminfo_tparm=yes --disable-openmp --with-included-libcroco --with-included-libglib --without-included-libxml"
