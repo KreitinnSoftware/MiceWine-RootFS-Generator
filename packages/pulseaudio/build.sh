a8f68efe0bf26730c2d466e53243c6c7ef740602
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-04-19 09:48:37 -0300

    Update build.sh

--- packages/pulseaudio/build.sh
+++ packages/pulseaudio/build.sh
@@ -3,4 +3,4 @@ SRC_URL=https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/archive/$PKG_VER/
 MESON_ARGS="-Dalsa=disabled -Dx11=disabled -Dgtk=disabled -Dopenssl=disabled -Dgsettings=disabled -Ddoxygen=false -Ddatabase=simple -Dsystemd=disabled -Dudev=disabled -Dgstreamer=disabled -Dglib=disabled -Dman=false -Dbashcompletiondir=false -Dzshcompletiondir=false -Dtests=false"
 CFLAGS="-I$PREFIX/include"
 CPPFLAGS="-I$PREFIX/include"
-LDFLAGS="-L$PREFIX/lib -Wl,--undefined-version"
+LDFLAGS="-L$PREFIX/lib -Wl,--undefined-version -l:libintl.a"
