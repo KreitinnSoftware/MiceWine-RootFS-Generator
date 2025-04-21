93d5a8425518da5884752fbbb4f766aff096bd56
KreitinnSoftware <pablolabs36@gmail.com>
2025-04-20 22:21:09 -0300

    Change 'libglvnd' source url

--- packages/libglvnd/build.sh
+++ packages/libglvnd/build.sh
@@ -1,4 +1,4 @@
 PKG_VER=1.7.0
-SRC_URL=https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/v$PKG_VER/libglvnd-v$PKG_VER.tar.gz
+SRC_URL=https://github.com/NVIDIA/libglvnd/archive/refs/tags/v$PKG_VER.tar.gz
 MESON_ARGS="-Dtls=false"
 CFLAGS="-I$PREFIX_DIR/include"
