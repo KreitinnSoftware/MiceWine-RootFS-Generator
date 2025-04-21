504f9d6dcbb329cc8d61e27548da6b6d495cf05b
KreitinnSoftware <pablolabs36@gmail.com>
2025-04-20 22:13:36 -0300

    Change 'pulseaudio' source location for Mirror on GitHub

--- packages/pulseaudio/build.sh
+++ packages/pulseaudio/build.sh
@@ -1,5 +1,5 @@
 PKG_VER=v17.0
-SRC_URL=https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/archive/$PKG_VER/pulseaudio-$PKG_VER.tar.gz
+SRC_URL=https://github.com/pulseaudio/pulseaudio/archive/refs/tags/$PKG_VER.tar.gz
 MESON_ARGS="-Dalsa=disabled -Dx11=disabled -Dgtk=disabled -Dopenssl=disabled -Dgsettings=disabled -Ddoxygen=false -Ddatabase=simple -Dsystemd=disabled -Dudev=disabled -Dgstreamer=disabled -Dglib=disabled -Dman=false -Dbashcompletiondir=false -Dzshcompletiondir=false -Dtests=false"
 CFLAGS="-I$PREFIX/include"
 CPPFLAGS="-I$PREFIX/include"
