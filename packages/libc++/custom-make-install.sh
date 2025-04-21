a93363d0a3aab13b6f6c6647569ee4df88800ab6
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-04-21 01:17:19 -0300

    Fix libc++ package

--- packages/libc++/custom-make-install.sh
+++ packages/libc++/custom-make-install.sh
@@ -1,3 +1,3 @@
 mkdir -p $DESTDIR/$PREFIX/lib
 
-cp $INIT_DIR/cache/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/$ARCHITECTURE-linux-android/libc++_shared.so $DESTDIR/$PREFIX/lib
+cp $INIT_DIR/cache/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/$ARCH-linux-android/libc++_shared.so $DESTDIR/$PREFIX/lib
