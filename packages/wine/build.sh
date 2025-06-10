a92448f3c47fc3ef508d02143b1f396cc824e135
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-06-09 23:15:06 -0300

    Update Wine (Add DInput Controller)

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,11 +1,11 @@
-PKG_VER="10.1-10-esync-xinput"
+PKG_VER="10.1-11-esync-xinput-dinput"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=8966a2e4ba20027aa956bdd08296543f4c7acafc
+GIT_COMMIT=1a4492046555dd13ddee5d7f12fa29f9ed47b697
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
