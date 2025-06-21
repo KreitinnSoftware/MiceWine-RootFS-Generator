9ea1d68e5b0f418283bbe32798c9365fb0bdbef0
KreitinnSoftware <pablolabs36@gmail.com>
2025-06-20 23:14:00 -0300

    Update Mesa Vulkan Wrapper to my Repository

--- packages/mesa-vulkan-wrapper/build.sh
+++ packages/mesa-vulkan-wrapper/build.sh
@@ -1,9 +1,9 @@
-PKG_VER=25.0.0-[gss]
+PKG_VER=25.1.4-[gss]
 PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Android Wrapper"
 VK_DRIVER_LIB="libvulkan_wrapper.so"
 
-GIT_URL=https://github.com/xMeM/mesa
-GIT_COMMIT=e65c7eb6ee2f9903c3256f2677beb1d98464103f
+GIT_URL=https://github.com/KreitinnSoftware/mesa
+GIT_COMMIT=aa4faf628dd477ea7b05f416b4d8da07f75ab399
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=enabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
