0e012132857064940fcf3dd59281ba6cf6a510c0
KreitinnSoftware <pablolabs36@gmail.com>
2025-08-18 22:44:38 -0300

    Update Base Wrapper for 'mesa-vulkan-wrapper-adrenotools'

--- packages/mesa-vulkan-wrapper-adrenotools/build.sh
+++ packages/mesa-vulkan-wrapper-adrenotools/build.sh
@@ -1,12 +1,12 @@
-PKG_VER=25.0.0-1-[gss]-adrenotools
-PKG_CATEGORY="AdrenoTools"
-PKG_PRETTY_NAME="Mesa Android Wrapper (AdrenoTools)"
+PKG_VER=25.1.4-[gss]-adrenotools
+PKG_CATEGORY="VulkanDriver"
+PKG_PRETTY_NAME="Mesa Android Wrapper"
 VK_DRIVER_LIB="libvulkan_wrapper.so"
 
 BLACKLIST_ARCH=x86_64
 
-GIT_URL=https://github.com/xMeM/mesa
-GIT_COMMIT=e65c7eb6ee2f9903c3256f2677beb1d98464103f
+GIT_URL=https://github.com/KreitinnSoftware/mesa
+GIT_COMMIT=7227cf46526196d54b22bd9ee53a265824f37420
 LDFLAGS="-L$PREFIX/lib -landroid-shmem -ladrenotools -llinkernsbypass"
-MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
-DEPENDENCIES="xorgproto libdrm libX11 libxcb libxshmfence Vulkan-Headers Vulkan-Loader zlib zstd libexpat libglvnd libpng libXext libXrandr libxshmfence libXxf86vm libadrenotools"
+MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=enabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
+DEPENDENCIES="xorgproto libdrm libX11 libxcb libxshmfence Vulkan-Headers Vulkan-Loader zlib zstd libexpat libglvnd libpng libXext libXrandr libXxf86vm libadrenotools"
