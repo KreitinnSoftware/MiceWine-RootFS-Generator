b6a253b362a6c3e568fefeed14f0cf79f2714a91
KreitinnSoftware <pablolabs36@gmail.com>
2025-08-20 17:52:39 +0000

    Add 'android-shmem' to 'libX11', 'libunbound', 'FreeGLUT'

--- packages/FreeGLUT/build.sh
+++ packages/FreeGLUT/build.sh
@@ -2,5 +2,5 @@ PKG_VER=3.4.0
 SRC_URL=https://downloads.sourceforge.net/freeglut/freeglut-$PKG_VER.tar.gz
 CMAKE_ARGS="-DANDROID=OFF -DCMAKE_LIBRARY_PATH=$PREFIX_DIR/lib"
 CFLAGS="-I$PREFIX_DIR/include"
-LDFLAGS="-L$PREFIX_DIR/lib -lGL"
-DEPENDENCIES="libX11 libXi libXrandr libXxf86vm GLU"
+LDFLAGS="-L$PREFIX_DIR/lib -lGL -landroid-shmem"
+DEPENDENCIES="libX11 libXi libXrandr libXxf86vm GLU android-shmem"
--- packages/libX11/build.sh
+++ packages/libX11/build.sh
@@ -1,4 +1,5 @@
 PKG_VER=1.8.9
 SRC_URL=https://xorg.freedesktop.org/releases/individual/lib/libX11-$PKG_VER.tar.xz
 CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE --enable-malloc0returnsnull"
-DEPENDENCIES="xorgproto libxcb xtrans xorg-utils-macros"
+LDFLAGS="-L$PREFIX/lib -landroid-shmem"
+DEPENDENCIES="xorgproto libxcb xtrans xorg-utils-macros android-shmem"
