c0e5061c5f274ec0d52dd356c78baa3dc5bc1019
Pablo Labs <80591934+KreitinnSoftware@users.noreply.github.com>
2025-11-20 15:11:43 -0300

    Update Wine to 10.10

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,11 +1,11 @@
-PKG_VER="10.1-12-esync-xinput-dinput"
+PKG_VER="10.10-esync-xinput-dinput"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=878fcd8452d4f27b634b0db802469a738d37270d
+GIT_COMMIT=ad2c2468a2cf3c39d8487bb3c08b3c4fb479d350
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
